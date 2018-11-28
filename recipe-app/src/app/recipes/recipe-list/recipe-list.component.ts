421204e5f94e36e1a1cf0bbb8a761a3ce13bbb73
talha <talhamucar@gmail.com>
2018-11-28 13:17:09 -0500

    add recipe model

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Description of the Test Recipe', 'https://media-cdn.tripadvisor.com/media/photo-s/0f/44/94/43/nefis-karisik-kebap.jpg'),
+    new Recipe('A Test Recipe', 'Description of the Test Recipe', 'https://media-cdn.tripadvisor.com/media/photo-s/0f/44/94/43/nefis-karisik-kebap.jpg')
+  ];
 
   constructor() { }
 
